33f36a8856eb03f17e96a26a220509108046b428
Lucas Santus <leos9877@gmail.com>
2021-03-17 10:29:57 -0300

    implementando models

--- contas/views.py
+++ contas/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 import datetime
 
 def home(request):
     now = datetime.datetime.now()
-    #html = "<html><body>Its now %s.</body></html>" % now
-    return render(request, 'home.hmtl')
+    return render(request, 'contas/home.html')
