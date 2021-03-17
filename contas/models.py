012460ac35af6f1da5b32124e9dd210ee08bee92
Lucas Santus <leos9877@gmail.com>
2021-03-17 13:57:14 -0300

    implementando view

--- contas/models.py
+++ contas/models.py
@@ -6,8 +6,11 @@ class Categoria(models.Model):
     nome = models.CharField(max_length=100)
     dt_criacao = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.nome
+        
 class Transacao(models.Model):
-    data = models.DateTimeField(auto_now_add=True)
+    data = models.DateTimeField()
     descricao = models.CharField(max_length=200)
     valor = models.DecimalField(max_digits=7, decimal_places=2)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
@@ -15,3 +18,6 @@ class Transacao(models.Model):
 
     class Meta:
         verbose_name_plural = "Transações"
+    
+    def __str__(self):
+        return self.descricao
--- contas/views.py
+++ contas/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 import datetime
 
 def home(request):
-    now = datetime.datetime.now()
-    return render(request, 'contas/home.html')
+    data = {}
+    data['transacoes'] = ['t1', 't2', 't3']
+    data['now'] = datetime.datetime.now()
+    return render(request, 'contas/home.html', data)
